e260ce0626e7c95623f4a5f0427e7df385154d01
psamodurov13 <psamodurov13@gmail.com>
2023-03-22 02:12:51 +0300

    created function for collect data

--- mugla_site/collect_data/collect.py
+++ mugla_site/collect_data/collect.py
@@ -3,12 +3,21 @@ from loguru import logger
 from bs4 import BeautifulSoup as bs
 
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import random as rd
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+import re
+import pprint
+
+
 
 headers = {
     'Referer': 'https://www.google.com/',
@@ -28,9 +37,10 @@ headers = {
 
 options = Options()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
 options.add_argument(f'user-agent={headers["User-Agent"]}')
-
-
+driver.fullscreen_window()
 
 
 def collect_data(keyword, location):
@@ -43,14 +53,14 @@ def collect_data(keyword, location):
     #     file.write(response.text)
     driver.get(f'https://www.google.com.tr/maps/search/{keyword.replace(" ", "+")}/@{location},13z')
     # driver.get('https://sweethomedress.ru/')
-    time.sleep(3)
+    time.sleep(2)
     #
     # footer = driver.find_element(By.CSS_SELECTOR, "div.dS8AEf")
     # delta_y = footer.rect['y']
     # ActionChains(driver).scroll_by_amount(0, delta_y).perform()
 
-    driver.execute_script('window.scrollTo(0, 650);')
-    time.sleep(2)
+    # driver.execute_script('window.scrollTo(0, 650);')
+    # time.sleep(1)
 
     SCROLL_PAUSE_TIME = 2
     # Get scroll height
@@ -70,20 +80,118 @@ def collect_data(keyword, location):
     #     if new_height == last_height:
     #         break
     #     last_height = new_height
-    loaded_companies = driver.find_elements(By.CSS_SELECTOR, 'div.bfdHYd')
+    loaded_companies = driver.find_elements(By.CSS_SELECTOR, 'div.THOPZb')
+    all_comp = [(i.find_element(By.CSS_SELECTOR, '.qBF1Pd'), i.find_element(By.CSS_SELECTOR, 'a.hfpxzc').get_attribute('href')) for i in loaded_companies]
+    logger.info(f'RESULT - {all_comp}')
+    # logger.info(f'HlvSq - {driver.find_element(By.CSS_SELECTOR, ".HlvSq")}')
+    result = []
+    count = 1
     while True:
-        driver.execute_script('arguments[0].scrollIntoView(true);', loaded_companies[-1])
-        all_companies = driver.find_elements(By.CSS_SELECTOR, 'div.bfdHYd')
-        logger.info(f'LENL - {len(loaded_companies)}')
+        # driver.execute_script("document.getElementsByClassName('m6QErb').scrollTop = document.getElementsByClassName('m6QErb').scrollHeight;")
+        time.sleep(rd.randint(1, 4))
+        all_companies = driver.find_elements(By.CSS_SELECTOR, 'div.THOPZb')
         logger.info(f'LENA - {len(all_companies)}')
-        if len(all_companies) == len(loaded_companies):
-            logger.info(f'FINALL - {len(loaded_companies)}')
-            logger.info(f'FINALA - {len(all_companies)}')
+        # result.extend(all_companies)
+        # logger.info(f'LENL - {len(loaded_companies)}')
+        try:
+            finish = driver.find_element(By.CSS_SELECTOR, ".HlvSq")
+            logger.info(f'FIN {finish.text}')
+        except Exception:
+            finish = False
+        if finish:
+            # driver.execute_script('arguments[0].scrollIntoView(true);', all_companies[-1])
+            # logger.info(f'FINALL - {len(loaded_companies)}')
+            all_companies = driver.find_elements(By.CSS_SELECTOR, 'div.THOPZb')
+            # result.extend(all_companies)
+            logger.info(f'FINAL-RES - {len(all_companies)}')
             break
         else:
-            loaded_companies = all_companies[:]
-            time.sleep(1)
-
+        #     # loaded_companies = all_companies[:]
+            logger.info(f'Прокрутка')
+            driver.execute_script('arguments[0].scrollIntoView(true);', all_companies[-1])
+            count += 1
+        #     # time.sleep(5)
+    time.sleep(2)
+    res = {}
+    c = 1
+    logger.info(f'AC - {all_companies}')
+    res = {i.find_element(By.CSS_SELECTOR, 'a.hfpxzc').get_attribute('href'): {} for i in all_companies}
+    for href in res.keys():
+        logger.info(f'C - {c}')
+        # logger.info(f'A - {href}')
+        # href = i.find_element(By.CSS_SELECTOR, 'a.hfpxzc').get_attribute('href')
+        logger.info(f'H - {href}')
+        driver.get(href)
+        time.sleep(1)
+        title = driver.find_element(By.CSS_SELECTOR, '.DUwDvf').text
+        try:
+            subtitle = driver.find_element(By.CSS_SELECTOR, '.bwoZTb').text
+        except NoSuchElementException:
+            subtitle = ''
+        category = driver.find_element(By.CSS_SELECTOR, 'button[jsaction="pane.rating.category"]').text
+        location = re.findall(r'\d{2}.\d{7}', href)
+        try:
+            address = driver.find_element(
+                By.CSS_SELECTOR, 'button[data-item-id="address"]'
+            ).get_attribute('aria-label').replace('Адрес: ', '')
+        except NoSuchElementException:
+            address = ''
+        try:
+            phone = driver.find_element(
+                By.CSS_SELECTOR, 'button[data-tooltip="Скопировать номер"]'
+            ).get_attribute('data-item-id').replace('phone:tel:', '')
+        except NoSuchElementException:
+            phone = ''
+        try:
+            time.sleep(2)
+            open_ = driver.find_elements(
+                By.CSS_SELECTOR, '.OqCZI .y0skZc'
+            )
+            logger.info(f'OPEN_ - {open_}')
+            open_hours = {}
+            for i in open_:
+                day = i.find_element(By.CSS_SELECTOR, '.HuudEc button').get_attribute('data-value').split(',')[0]
+                logger.info(f'DAY - {day}')
+                # logger.info(f'DAY TEXT - {day.text}')
+                hours = i.find_element(By.CSS_SELECTOR, '.mxowUb').get_attribute('aria-label')
+                logger.info(f'HOURS - {hours}')
+                open_hours[day] = hours
+        except NoSuchElementException:
+            open_hours = {}
+        try:
+            tags_ = driver.find_elements(By.CSS_SELECTOR, '.E0DTEd .LTs0Rc')
+            tags = []
+            for i in tags_:
+                status = i.find_element(By.CSS_SELECTOR, '.TRbhbd').get_attribute('src')
+                if status == 'https://maps.gstatic.com/consumer/images/icons/2x/ic_done_18px.png':
+                    tags.append(i.find_element(By.CSS_SELECTOR, 'div[jstcache="72"]').text)
+        except NoSuchElementException:
+            tags = []
+        try:
+            photos = driver.find_element(By.CSS_SELECTOR, 'button.ofKBgf[aria-label="Все"]')
+            driver.execute_script('arguments[0].scrollIntoView(true);', photos)
+            photos.click()
+            time.sleep(5)
+            photos_tags = driver.find_elements(By.CSS_SELECTOR, '.Uf0tqf')
+            # styles = [i.get_attribute('style') for i in photos_tags]
+            photos_urls = []
+            for i in photos_tags:
+                try:
+                    driver.execute_script('arguments[0].scrollIntoView(true);', i)
+                    time.sleep(1)
+                    photos_urls.append(re.search(r'url\("(.*?)"\)', i.get_attribute('style')).group(1))
+                except AttributeError:
+                    logger.info(f'Photo error - {i}')
+                    time.sleep(1)
+        except NoSuchElementException:
+            photos_urls = []
+        res[href] = {'title': title, 'subtitle': subtitle, 'category': category, 'location': location,
+                     'address': address, 'phone': phone, 'open_hours': open_hours, 'tags': tags,
+                     'photos_urls': photos_urls}
+        for k, v in res[href].items():
+            logger.info(f'{k} - {v}')
+        c += 1
+    pprint.pprint(res)
 
 
 if __name__ == '__main__':
