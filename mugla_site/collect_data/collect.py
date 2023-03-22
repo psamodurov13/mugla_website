85db9f6c7a8cbd4d2ee7e8f84f6c5df71b4179a3
psamodurov13 <psamodurov13@gmail.com>
2023-03-23 01:13:10 +0300

    created function for load data from collect to database

--- mugla_site/collect_data/collect.py
+++ mugla_site/collect_data/collect.py
@@ -16,7 +16,8 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 import re
 import pprint
-
+from selenium.webdriver.common.action_chains import ActionChains
+from telegram_notifications.tg_notification import notification
 
 
 headers = {
@@ -36,14 +37,19 @@ headers = {
 }
 
 options = Options()
+options.add_argument('headless')
+options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.maximize_window()
+
 # driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 options.add_argument(f'user-agent={headers["User-Agent"]}')
-driver.fullscreen_window()
+# driver.fullscreen_window()
 
 
 def collect_data(keyword, location):
+    notification(f'Старт парсинга - {keyword}')
     # url = f'https://www.google.com.tr/maps/search/{keyword.replace(" ", "+")}/@{location},13z'
     # response = rq.get(url, headers=headers)
     # logger.info(f'RESPONSE - {response}')
@@ -86,10 +92,14 @@ def collect_data(keyword, location):
     # logger.info(f'HlvSq - {driver.find_element(By.CSS_SELECTOR, ".HlvSq")}')
     result = []
     count = 1
+    action = ActionChains(driver)
     while True:
         # driver.execute_script("document.getElementsByClassName('m6QErb').scrollTop = document.getElementsByClassName('m6QErb').scrollHeight;")
         time.sleep(rd.randint(1, 4))
+        left_side = driver.find_element(By.CSS_SELECTOR, '.XltNde')
         all_companies = driver.find_elements(By.CSS_SELECTOR, 'div.THOPZb')
+        action.move_to_element(all_companies[0])
+        action.perform()
         logger.info(f'LENA - {len(all_companies)}')
         # result.extend(all_companies)
         # logger.info(f'LENL - {len(loaded_companies)}')
@@ -115,86 +125,100 @@ def collect_data(keyword, location):
     res = {}
     c = 1
     logger.info(f'AC - {all_companies}')
-    res = {i.find_element(By.CSS_SELECTOR, 'a.hfpxzc').get_attribute('href'): {} for i in all_companies}
+    res = {
+        i.find_element(By.CSS_SELECTOR, 'a.hfpxzc').get_attribute('href').replace('&hl=tr&', '&hl=ru&'): {}
+        for i in all_companies
+    }
     for href in res.keys():
         logger.info(f'C - {c}')
         # logger.info(f'A - {href}')
         # href = i.find_element(By.CSS_SELECTOR, 'a.hfpxzc').get_attribute('href')
         logger.info(f'H - {href}')
-        driver.get(href)
-        time.sleep(1)
-        title = driver.find_element(By.CSS_SELECTOR, '.DUwDvf').text
         try:
-            subtitle = driver.find_element(By.CSS_SELECTOR, '.bwoZTb').text
-        except NoSuchElementException:
-            subtitle = ''
-        category = driver.find_element(By.CSS_SELECTOR, 'button[jsaction="pane.rating.category"]').text
-        location = re.findall(r'\d{2}.\d{7}', href)
-        try:
-            address = driver.find_element(
-                By.CSS_SELECTOR, 'button[data-item-id="address"]'
-            ).get_attribute('aria-label').replace('Адрес: ', '')
-        except NoSuchElementException:
-            address = ''
-        try:
-            phone = driver.find_element(
-                By.CSS_SELECTOR, 'button[data-tooltip="Скопировать номер"]'
-            ).get_attribute('data-item-id').replace('phone:tel:', '')
-        except NoSuchElementException:
-            phone = ''
-        try:
-            time.sleep(2)
-            open_ = driver.find_elements(
-                By.CSS_SELECTOR, '.OqCZI .y0skZc'
-            )
-            logger.info(f'OPEN_ - {open_}')
-            open_hours = {}
-            for i in open_:
-                day = i.find_element(By.CSS_SELECTOR, '.HuudEc button').get_attribute('data-value').split(',')[0]
-                logger.info(f'DAY - {day}')
-                # logger.info(f'DAY TEXT - {day.text}')
-                hours = i.find_element(By.CSS_SELECTOR, '.mxowUb').get_attribute('aria-label')
-                logger.info(f'HOURS - {hours}')
-                open_hours[day] = hours
-        except NoSuchElementException:
-            open_hours = {}
-        try:
-            tags_ = driver.find_elements(By.CSS_SELECTOR, '.E0DTEd .LTs0Rc')
-            tags = []
-            for i in tags_:
-                status = i.find_element(By.CSS_SELECTOR, '.TRbhbd').get_attribute('src')
-                if status == 'https://maps.gstatic.com/consumer/images/icons/2x/ic_done_18px.png':
-                    tags.append(i.find_element(By.CSS_SELECTOR, 'div[jstcache="72"]').text)
-        except NoSuchElementException:
-            tags = []
-        try:
-            photos = driver.find_element(By.CSS_SELECTOR, 'button.ofKBgf[aria-label="Все"]')
-            driver.execute_script('arguments[0].scrollIntoView(true);', photos)
-            photos.click()
-            time.sleep(5)
-            photos_tags = driver.find_elements(By.CSS_SELECTOR, '.Uf0tqf')
-            # styles = [i.get_attribute('style') for i in photos_tags]
-            photos_urls = []
-            for i in photos_tags:
-                try:
-                    driver.execute_script('arguments[0].scrollIntoView(true);', i)
-                    time.sleep(1)
-                    photos_urls.append(re.search(r'url\("(.*?)"\)', i.get_attribute('style')).group(1))
-                except AttributeError:
-                    logger.info(f'Photo error - {i}')
-                    time.sleep(1)
-        except NoSuchElementException:
-            photos_urls = []
-        res[href] = {'title': title, 'subtitle': subtitle, 'category': category, 'location': location,
-                     'address': address, 'phone': phone, 'open_hours': open_hours, 'tags': tags,
-                     'photos_urls': photos_urls}
-        for k, v in res[href].items():
-            logger.info(f'{k} - {v}')
+            driver.get(href)
+            time.sleep(1)
+            title = driver.find_element(By.CSS_SELECTOR, '.DUwDvf').text
+            try:
+                subtitle = driver.find_element(By.CSS_SELECTOR, '.bwoZTb').text
+            except NoSuchElementException:
+                subtitle = ''
+            category = driver.find_element(By.CSS_SELECTOR, 'button[jsaction="pane.rating.category"]').text
+            location = re.findall(r'\d{2}.\d{7}', href)
+            try:
+                address = driver.find_element(
+                    By.CSS_SELECTOR, 'button[data-item-id="address"]'
+                ).get_attribute('aria-label').replace('Адрес: ', '')
+            except NoSuchElementException:
+                address = ''
+            try:
+                phone = driver.find_element(
+                    By.CSS_SELECTOR, 'button[data-tooltip="Скопировать номер"]'
+                ).get_attribute('data-item-id').replace('phone:tel:', '')
+            except NoSuchElementException:
+                phone = ''
+            try:
+                website = driver.find_element(By.CSS_SELECTOR, '.ITvuef .Io6YTe').text
+            except NoSuchElementException:
+                website = ''
+            try:
+                time.sleep(2)
+                open_ = driver.find_elements(
+                    By.CSS_SELECTOR, '.OqCZI .y0skZc'
+                )
+                logger.info(f'OPEN_ - {open_}')
+                open_hours = {}
+                for i in open_:
+                    day = i.find_element(By.CSS_SELECTOR, '.HuudEc button').get_attribute('data-value').split(',')[0]
+                    logger.info(f'DAY - {day}')
+                    # logger.info(f'DAY TEXT - {day.text}')
+                    hours = i.find_element(By.CSS_SELECTOR, '.mxowUb').get_attribute('aria-label')
+                    logger.info(f'HOURS - {hours}')
+                    open_hours[day] = hours
+            except NoSuchElementException:
+                open_hours = {}
+            try:
+                tags_ = driver.find_elements(By.CSS_SELECTOR, '.E0DTEd .LTs0Rc')
+                tags = []
+                for i in tags_:
+                    status = i.find_element(By.CSS_SELECTOR, '.TRbhbd').get_attribute('src')
+                    if status == 'https://maps.gstatic.com/consumer/images/icons/2x/ic_done_18px.png':
+                        tags.append(i.find_element(By.CSS_SELECTOR, 'div[jstcache="72"]').text)
+            except NoSuchElementException:
+                tags = []
+            try:
+                photos = driver.find_element(By.CSS_SELECTOR, 'button.ofKBgf[aria-label="Все"]')
+                driver.execute_script('arguments[0].scrollIntoView(true);', photos)
+                photos.click()
+                time.sleep(5)
+                photos_tags = driver.find_elements(By.CSS_SELECTOR, '.Uf0tqf')
+                # styles = [i.get_attribute('style') for i in photos_tags]
+                photos_urls = []
+                for i in photos_tags:
+                    try:
+                        driver.execute_script('arguments[0].scrollIntoView(true);', i)
+                        time.sleep(1)
+                        photos_urls.append(re.search(r'url\("(.*?)"\)', i.get_attribute('style')).group(1))
+                    except AttributeError:
+                        logger.info(f'Photo error - {i}')
+                        time.sleep(1)
+            except NoSuchElementException:
+                photos_urls = []
+            res[href] = {'title': title, 'subtitle': subtitle, 'category': category, 'location': location,
+                         'address': address, 'phone': phone, 'website': website, 'open_hours': open_hours, 'tags': tags,
+                         'photos_urls': photos_urls}
+            for k, v in res[href].items():
+                logger.info(f'{k} - {v}')
+        except Exception:
+            logger.error(f'Ошибка сбора данных о компании {href}')
+            logger.exception(Exception)
         c += 1
     pprint.pprint(res)
+    notification(f'Парсинг компаний выполнен. Всего - {len(res)}')
+    return res
 
 
 if __name__ == '__main__':
     collect_data('anaokulu marmaris', '36.8611888,28.2643651')
+    # collect_data('restaurants marmaris', '36.8611888,28.2643651')
     # collect_data('', '')
     logger.info('DONE')
---
+++ mugla_site/collect_data/load.py
@@ -0,0 +1,84 @@
+# from django.conf import settings
+#
+# settings.configure()
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mugla_site.settings")
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+from django.contrib.auth.models import User
+
+
+from transliterate import slugify
+
+from companies.models import Company, Type
+
+
+res = {
+    'https://www.google.com.tr/maps/place/MEB+%C3%96zel+Marmaris+Pinokyo+Anaokulu/data=!4m7!3m6!1s0x14bfcb5142d100e1:0xdadb263b0de5686a!8m2!3d36.8569963!4d28.2629144!16s%2Fg%2F1tp1dh0b!19sChIJ4QDRQlHLvxQRamjlDTsm29o?authuser=0&hl=ru&rclk=1': {
+        'address': 'Kemeraltı, '
+                   '112. '
+                   'Sk. '
+                   'No:27, '
+                   '48700 '
+                   'Marmaris/Muğla ',
+        'category': 'Детский '
+                    'сад',
+        'location': ['36.8569963',
+                     '28.2629144'],
+        'open_hours': {'воскресенье': 'Закрыто',
+                       'вторник': '07:30–17:30',
+                       'понедельник': '07:30–17:30',
+                       'пятница': '07:30–17:30',
+                       'среда': '07:30–17:30',
+                       'суббота': 'Закрыто',
+                       'четверг': '07:30–17:30'},
+        'phone': '05321709812',
+        'photos_urls': ['https://lh5.googleusercontent.com/p/AF1QipMTt4E8Cq_Ayp49rtwWZAD6qzyd_b5p0RWOLve-=s878-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipNKy568pJTq6wsm5iR3jsSNDOJkISnPhGRRwsyu=s1105-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipOZ_LreYSc8g5vYNQGXMD1337nuCB-Hywrx6dEV=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipMFcWSL7XWnhZ-EY48V8lrhxg8cmfH6ctFY94HB=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipPQOOKlYFqm4mHMoypoAdSeAksRuE-pfG0A_igh=s871-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipM49L29UVUoNzwFU4BrYmwu3NAmu5vkz_kT3IfF=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipNI-FasHjTouoELght88BJLXVRgihc-dcGIWuum=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipMlmSM29f6hGP-kvvUsL8KDeltxrYJ7Mh7JIed3=s873-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipM91pGysY_1oZb2Rg5NE5qPk9myO2-Nfuevr9qy=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipN4sa3YWfhABKgnV1-9KaIkXFOtC7NtLiKnj66D=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipMr-GGfCVVIkPQw2OYmZ2QW5omxgkVc0qp-AnsN=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipOdmPKtOvFAB7d8D7RzXd2wTFoTwHyjW2CqLbHm=s1022-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipNjFz3mHOXor7tN3Trey8MjvsQ5ZqerL-ztGuAx=s869-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipPb-tu2c0hi1yBAC7UoGiTSfSuZHl0pKXdwmSvA=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipMSqfWtFJBbhI-W6tx76-DbM3Yto7JnLxEI1Txu=s1013-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipPO8_d1RnicTeDe1emhgw1PcHQUD7jIdtqcNHuA=s1031-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipO0azADVyz1W4cMFFvFRUAkmYEnZQlv2UMyVx45=s869-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipPbGGvTuocx-cJoOjobU6KaawU6qSqjXelDcft0=s812-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipPs3htz1tk6SNB9fRWoFp4oKujaANc84C5Y-A_9=s869-k-no',
+                        'https://lh5.googleusercontent.com/p/AF1QipP6J6l2fPuoTztXUB31Hq98i3-c_u_aLn-u_lrn=s1354-k-no'],
+        'subtitle': 'MEB '
+                    'Özel '
+                    'Marmaris '
+                    'Pinokyo '
+                    'Anaokulu',
+        'tags': [],
+        'title': 'Ozel Marmaris Pinokyo Anaokulu',
+        'website': ''},
+}
+
+
+def load_companies(res):
+    for i in res.values():
+        last_id = Company.objects.order_by('id').last().id
+        slug = slugify(i['title'], language_code='ru') + str(last_id + 1)
+
+        company = Company.objects.create(author=User.objects.get_by_natural_key('psamodurov13'),
+                                         type=Type.objects.get(title='Образование'),
+                                         slug=slug)
+        company.title = i['title']
+        # company.type = 1
+        company.content = ''
+        # company.author = User.objects.get_by_natural_key('psamodurov13')
+        company.save()
+
+
+if __name__ == '__main__':
+    load_companies(res)
+
